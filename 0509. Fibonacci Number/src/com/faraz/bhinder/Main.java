23b33c56fa3ea2b798d348238164fe2467761e82
Faraz Ahmad <farazbhinder@users.noreply.github.com>
2021-12-07 20:05:14 +0500

    feat: add iterative version

--- 0509. Fibonacci Number/src/com/faraz/bhinder/Main.java
+++ 0509. Fibonacci Number/src/com/faraz/bhinder/Main.java
@@ -11,6 +11,11 @@ public class Main {
         for (int i = 0; i < 15; i++) {
             System.out.println(solution.fib(i));
         }
+
+        Solution2 solution2 = new Solution2();
+        for (int i = 0; i < 15; i++) {
+            System.out.println(solution2.fib(i));
+        }
     }
 }
 
@@ -30,6 +35,21 @@ class Solution {
         int res = fib(n-1) + fib(n-2);
         map.put(n, res);
         return res;
+    }
+}
 
+class Solution2 {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public int fib(int n) {
+        map.put(0, 0);
+        map.put(1, 1);
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        for (int i = 2; i <= n; i++) {
+            map.put(i, map.get(i-1) + map.get(i-2));
+        }
+        return map.get(n);
     }
 }
