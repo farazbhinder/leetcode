9ad3e32c89d9e5b938650372c687581194b47300
Faraz Ahmad <farazahmad@yandex.com>
2023-09-29 16:34:53 +0500

    add solution for 1081 (duplicate of 0316)

---
+++ 1081. Smallest Subsequence of Distinct Characters/smallest-subsequence-distinct-chars/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    // 1081. Smallest Subsequence of Distinct Characters (same as 0316)
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.smallestSubsequence("thesqtitxyetpxloeevdeqifkz");
+
+        System.out.println(result);
+    }
+}
