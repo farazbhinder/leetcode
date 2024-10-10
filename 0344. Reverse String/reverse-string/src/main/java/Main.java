079ff083ead7bd3586b72b479ad1569124513018
Faraz Ahmad <farazahmad@yandex.com>
2024-10-11 00:47:32 +0500

    add code for 344. reverse string

---
+++ 0344. Reverse String/reverse-string/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] s = "hello".toCharArray();
+        Solution solution = new Solution();
+        solution.reverseString(s);
+        System.out.println(s);
+    }
+}
