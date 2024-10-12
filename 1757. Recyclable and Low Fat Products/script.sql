f008c2d173e286cce112a296eb8c8386e12077a8
Faraz Ahmad <farazahmad@yandex.com>
2024-10-13 00:17:48 +0500

    add solution

---
+++ 1757. Recyclable and Low Fat Products/script.sql
@@ -0,0 +1,19 @@
+-- postgres
+-- Create table if it does not exist using CHAR for low_fats and recyclable
+CREATE TABLE IF NOT EXISTS Products (
+    product_id INT,
+    low_fats CHAR(1),
+    recyclable CHAR(1)
+);
+
+-- Truncate the Products table
+TRUNCATE TABLE Products;
+
+-- Insert values into the Products table
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (0, 'Y', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (1, 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (2, 'N', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (3, 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (4, 'N', 'N');
+
+select p.product_id from products p where p.low_fats = 'Y' and p.recyclable = 'Y';
